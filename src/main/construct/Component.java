4f177b132babeb339ed486fdbec8415bd76ce043
Armo777 <arman.sarkisyan.03@mail.ru>
2023-03-15 23:22:16 +0300

    Добавил класс Component

---
+++ src/main/construct/Component.java
@@ -0,0 +1,12 @@
+package main.construct;
+
+public class Component {
+    public static int MAUONNAISE = 100;
+    public static int MUSTERD = 100;
+    public static int KETCHUP = 100;
+    public static int ONION = 100;
+    public static int JALAPENO = 100;
+    public static int CHILE = 100;
+    public static int CUCUMBER = 100;
+
+}
